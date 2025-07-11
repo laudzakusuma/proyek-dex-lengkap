a72167dc5e009fef64a03a231bbde4497c79ef18
laudzakusuma <laudzaxie@gmail.com>
2025-07-11 21:11:29 +0700

    s

---
+++ client/src/pages/ChartPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChartPage = () => {
+  return (
+    <div style={{color: 'white', animation: 'fadeInUp 0.5s ease-out'}}>
+      <h2>Halaman Chart</h2>
+      <p>Grafik harga token akan ditampilkan di sini.</p>
+    </div>
+  );
+};
+
+export default ChartPage;
