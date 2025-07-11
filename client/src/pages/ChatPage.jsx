0ecef98e23447b024982bda723bc8ffb765017aa
laudzakusuma <laudzaxie@gmail.com>
2025-07-11 21:11:53 +0700

    s

---
+++ client/src/pages/ChatPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChatPage = () => {
+  return (
+    <div style={{color: 'white', animation: 'fadeInUp 0.5s ease-out'}}>
+      <h2>Halaman Chat</h2>
+      <p>Fitur komunitas chat akan ditampilkan di sini.</p>
+    </div>
+  );
+};
+
+export default ChatPage;
