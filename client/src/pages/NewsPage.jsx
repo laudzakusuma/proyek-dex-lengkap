75c98515fecc8eb6c59f042c6800d5a5088eaacf
laudzakusuma <laudzaxie@gmail.com>
2025-07-11 21:11:01 +0700

    s

---
+++ client/src/pages/NewsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewsPage = () => {
+  return (
+    <div style={{color: 'white', animation: 'fadeInUp 0.5s ease-out'}}>
+      <h2>Halaman Berita</h2>
+      <p>Konten berita akan ditampilkan di sini.</p>
+    </div>
+  );
+};
+
+export default NewsPage;
